c5680929c359a46532ab05153d49bf694531a1cd
EnesUzun03 <161275404+EnesUzun03@users.noreply.github.com>
2025-03-13 02:31:05 +0300

    Week2 examples

---
+++ src/main/java/com/enesuzun/_2_week/Week2_Examples_1.java
@@ -0,0 +1,24 @@
+package com.enesuzun._2_week;
+
+import java.util.Scanner;
+
+public class Week2_Examples_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sayi = 0;
+        while (true) {
+            System.out.println("Lütfen bir sayi giriniz");
+            sayi = scanner.nextInt();
+
+            int kalan = sayi % 2;
+            if (kalan == 0) {
+                System.out.println("Çift");
+            } else {
+                System.out.println("Tek");
+            }
+
+        }
+
+    }
+}
