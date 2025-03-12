289d5646d22bb025b64d753e033804fc4f79bb18
EnesUzun03 <161275404+EnesUzun03@users.noreply.github.com>
2025-03-13 01:23:01 +0300

    Conditional

---
+++ src/main/java/com/enesuzun/_2_week/_09_3_Conditional_ternary.java
@@ -0,0 +1,9 @@
+package com.enesuzun._2_week;
+
+public class _09_3_Conditional_ternary {
+    public static void main(String[] args) {
+        int number=5;
+        String sonuc = (number>0) ?"positif":"negatif";
+        System.out.println(sonuc);
+    }
+}
