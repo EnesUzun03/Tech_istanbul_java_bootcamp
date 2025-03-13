67faf4e307d20af206fd95887739ed75a42c646b
EnesUzun03 <161275404+EnesUzun03@users.noreply.github.com>
2025-03-13 07:37:30 +0300

    Methods

---
+++ src/main/java/com/enesuzun/_2_week/_13_1_MethodIsNotReturn.java
@@ -0,0 +1,30 @@
+package com.enesuzun._2_week;
+
+
+public class _13_1_MethodIsNotReturn {
+    //Metotlar (Returnsuz Parametresiz)
+    public void metotParametresiz(){
+        System.out.println("metotParametresiz");
+    }
+
+    //Metotlar (Returnsuz Parametreli)
+    public static void metotParametreli(String name){
+        System.out.println("metotParametresiz"+name);
+    }
+    //Metotlar (Returnsuz Parametreli)
+
+    public static void metotParametreli(String name,int age){
+        System.out.println("metotParametresiz"+name + " " + age);
+    }
+    //new
+    //static
+    public static void main(String[] args) {
+        _13_1_MethodIsNotReturn data1=new _13_1_MethodIsNotReturn();
+        data1.metotParametresiz();
+
+        //intance yapısı olmadan çağırma (new olmadan çağırma)
+        _13_1_MethodIsNotReturn.metotParametreli("Enes UZUN");
+        _13_1_MethodIsNotReturn.metotParametreli("Enes UZUN" ,22);
+    }
+
+}
