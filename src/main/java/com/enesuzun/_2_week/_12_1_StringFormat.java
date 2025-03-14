df6000a36bb847bb35b5d3d77491e7d044479534
EnesUzun03 <161275404+EnesUzun03@users.noreply.github.com>
2025-03-14 20:09:03 +0300

    Formatter

--- src/main/java/com/enesuzun/_2_week/_12_1_StringFormat.java
+++ src/main/java/com/enesuzun/_2_week/_12_1_StringFormat.java
@@ -8,9 +8,28 @@ public class _12_1_StringFormat {
         Formatter formatter=new Formatter();
         formatter.format("Merhaba %s Numaran %d TC : %d Fiyat %f","enes",216,1145668997,99.99);
         System.out.println(formatter);
+        formatter.close();//belleği serbest bırak
+        /* Garbarage collectors
+        * Yalnızca ama yalnızca kapalı olamayan dosya (scanner ,formatter gibi) Eğer biz kapatmazsak bu kap
+        * kapanmayan dosyayı temizlemeyebilir ancak açık olan dosyayı kapatmazsak cache belleği kullanmaya devam eder
+        */
+    }
+
+    //formatter 2
+    public static void formatter2(){
+        //Formatter formatter=new Formatter();
+        String formatterString=String.format("Merhaba %s Numaran %d TC : %d Fiyat %f","enes",216,1145668997,99.99);
+        System.out.println(formatterString);
+    }
+    //formatter 3
+    public static void formatter3(){
+
     }
 
     public static void main(String[] args) {
-        formatter1();
+        //formatter1();
+        formatter2();
     }
+
+
 }
