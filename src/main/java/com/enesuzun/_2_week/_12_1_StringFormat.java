e7aef886648334dc6c041e3330e83d8781b1a9a7
EnesUzun03 <161275404+EnesUzun03@users.noreply.github.com>
2025-03-14 19:54:57 +0300

    Formatter

--- src/main/java/com/enesuzun/_2_week/_12_1_StringFormat.java
+++ src/main/java/com/enesuzun/_2_week/_12_1_StringFormat.java
@@ -1,5 +1,16 @@
 package com.enesuzun._2_week;
 
-public class _12_1_StringFormat {
+import java.util.Formatter;
 
+public class _12_1_StringFormat {
+    //formatter1
+    public static void formatter1(){
+        Formatter formatter=new Formatter();
+        formatter.format("Merhaba %s Numaran %d TC : %d Fiyat %f","enes",216,1145668997,99.99);
+        System.out.println(formatter);
+    }
+
+    public static void main(String[] args) {
+        formatter1();
+    }
 }
--- src/main/java/com/enesuzun/_2_week/_15_3_LocalDateTime.java
+++ src/main/java/com/enesuzun/_2_week/_15_3_LocalDateTime.java
@@ -19,21 +19,24 @@ public class _15_3_LocalDateTime {
         System.out.println("Şu anda zaman : "+now.format(dateTimeFormatter));
     }
     //LocalDateTime 2
-    public static void localDateFormat2(){
-        LocalDateTime now=LocalDateTime.now();
-        Locale locale=new Locale("tr","TR");
-        //DateTimeFormatter dateTimeFormatter=DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss");
-        DateTimeFormatter dateTimeFormatter=DateTimeFormatter.ofPattern("yyyy-MMMM-dd HH-mm-ss");
-        //normal çıktı
-        System.out.println("Şu anda zaman : "+now);
-        //bizim formatımız
-        System.out.println("Şu anda zaman : "+now.format(dateTimeFormatter));
+    public static void localDateFormatSetMethod2() {
+        Locale locale = new Locale("tr", "TR");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH:mm:ss", locale);
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime newData= now
+                .withDayOfMonth(11) //
+                .withMonth(12)  //
+                .withYear(2029)
+                .withHour(14)
+                .withMinute(44)
+                .withSecond(23);
+        //System.out.println(now);
+        System.out.println("Normal Tarih: "+now);
+        System.out.println("Değiştirilmiş Tarih: "+newData.format(formatter));
     }
-
-
-
-
     public static void main(String[] args) {
-        localDateFormat();
+        //localDateFormat();
+        localDateFormatSetMethod2();
     }
 }
