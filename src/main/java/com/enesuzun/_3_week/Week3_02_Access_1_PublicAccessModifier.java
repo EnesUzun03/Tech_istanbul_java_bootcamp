3b13f795a6f7d93b8970c2e67d69305690675284
EnesUzun03 <161275404+EnesUzun03@users.noreply.github.com>
2025-03-15 17:21:54 +0300

    erişim belirteçleri

---
+++ src/main/java/com/enesuzun/_3_week/Week3_02_Access_1_PublicAccessModifier.java
@@ -0,0 +1,16 @@
+package com.enesuzun._3_week;
+
+public class Week3_02_Access_1_PublicAccessModifier {
+    public String publicData="public data";
+    private String privateData="private data";
+    protected String protectedData="protected data";
+    String defaultData="default data";
+
+    public static void main(String[] args) {
+        Week3_02_Access_1_PublicAccessModifier accessModifier=new Week3_02_Access_1_PublicAccessModifier();
+        System.out.println(accessModifier.publicData);
+        System.out.println(accessModifier.privateData);
+        System.out.println(accessModifier.protectedData);
+        System.out.println(accessModifier.defaultData);
+    }
+}
---
+++ src/main/java/com/enesuzun/_3_week/Week3_04_Class_POJO.java
@@ -0,0 +1,4 @@
+package com.enesuzun._3_week;
+
+public class Week3_04_Class_POJO {
+}
