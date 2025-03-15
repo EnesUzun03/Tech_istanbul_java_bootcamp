e181437db4117f8d06e66d1d231dfdc621ee2c3e
EnesUzun03 <161275404+EnesUzun03@users.noreply.github.com>
2025-03-15 17:34:36 +0300

    Pojo

---
+++ src/main/java/com/enesuzun/_3_week/Week3_05_Class_BEAN.java
@@ -0,0 +1,12 @@
+package com.enesuzun._3_week;
+
+public class Week3_05_Class_BEAN {
+    //field
+    private String name;
+    private String surname;
+
+    //Constructor(paramtresiz)
+    //Constructor(paramtreli)
+    //method
+    //getter setter
+}
